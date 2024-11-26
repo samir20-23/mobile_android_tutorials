d6a9b5ce50c29d5e8565f867dd74d9e8649dc826
samir20-23 <germanysamir1@gmail.com>
2024-11-26 11:26:15 +0000

    'new'

--- mobile_project_v9/app/src/main/java/com/example/mobile_project_v9/MainActivity.kt
+++ mobile_project_v9/app/src/main/java/com/example/mobile_project_v9/MainActivity.kt
@@ -1,5 +1,5 @@
 package com.example.mobile_project_v9.mobile_android_tutorials
-
+//test
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
